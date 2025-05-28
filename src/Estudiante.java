021adfa56af07ac17788e05eabd814c969afafa5
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-05-27 21:12:09 -0600

    se realizo el getter y setter para Notas,dentro de ellos hay un ciclo for el cual nos ayudara a ir moviendonos en el arreglo

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -23,5 +23,20 @@ public class Estudiante {
         this.matricula = matricula;
     }
 
+    public double getNota(int i) {
+        if (i >= 0 && i < notas.length) {
+            return notas[i];
+        }
+        System.out.println("Índice de nota fuera de rango");
+        return 0;
+    }
+
+    public void setNota(int i, double n) {
+        if (i >= 0 && i < notas.length) {
+            notas[i] = n;
+        } else {
+            System.out.println("Índice de nota fuera de rango");
+     }
+    }
 
 }

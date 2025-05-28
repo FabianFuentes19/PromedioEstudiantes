250e196953f4c547413cfa2ae16dd5c82040712a
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-05-27 21:52:38 -0600

    en el archivo main, se hizo un ciclo for el cual se repetira 5 veces, ya que es el numero de estudiantes deseados, dentro del ciclo se pedira el nombre y la matricula del mismo. Dentro de otro ciclo for se pediran 5 calificaciones, cuando el usuario termine de ingresar esas calificiones se realizara el promedio y se seguira con el siguiente estudiante

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Estudiante[] estudiantes = new Estudiante[5];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            Estudiante e = new Estudiante();
+
+            System.out.print("Ingrese el nombre del estudiante " + i + ": ");
+            e.setNombre(sc.nextLine());
+
+            System.out.print("Ingrese la matrícula del estudiante " + i + ": ");
+            e.setMatricula(sc.nextLine());
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print("Ingrese la calificación " + (j + 1) + ": ");
+                double nota = sc.nextDouble();
+                e.setNota(j, nota);
+            }
+            sc.nextLine(); 
+
+        }
+
+        sc.close();
     }
+
 }
